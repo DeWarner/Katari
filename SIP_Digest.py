caca2ff402bece0e2c3bb2d31b27f22a25c9c2ce
Aaron Parfitt <aaronparfitt@Aarons-MacBook-Pro.local>
2018-01-04 23:13:22 +0000

    Created class which deals with authorization challange

---
+++ SIP_Digest.py
@@ -0,0 +1,12 @@
+import hashlib
+class SIP_Digest:
+    
+    def Digest_Gen(self,username,password,realm,method,bwnonce,cnonce,uri):
+         count = "00000001"
+         auth = "auth"
+         ha1 = hashlib.md5(""+username+":"+realm+":"+password+"").hexdigest()
+         ha2 = hashlib.md5('%s:%s'%(method,uri)).hexdigest()
+         responce = hashlib.md5("%s:%s:%s:%s:%s:%s"%(ha1,bwnonce,count,cnonce,auth,ha2)).hexdigest()
+         return responce
+
+     
