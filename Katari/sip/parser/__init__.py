e25fc2b80cf276eaf9ae9ca30cb315bac5ca1461
dewarner <dec.warner@gmail.com>
2019-04-14 23:30:49 +0100

    adding naive parser function

--- Katari/sip/parser/__init__.py
+++ Katari/sip/parser/__init__.py
@@ -1,20 +1,35 @@
 from Katari.sip import SipMessage
 
-class SipParser:
 
-    def __init__(self,sip_message):
-        return self._parse(sip_message)
+class ParsedMessage:
+    def __init__(message):
+        self.message = message
+        self.info = parse(message)
 
 
-    def _parse(self, sip_message):
-        """
-        Takes in a sip message
+def parse(sip_message):
+    """
+    Takes in a sip message
+    
+    :param sip_message:
+    :return: dict
 
-        :param sip_message:
-        :return:
-        """
-
-        return SipMessage(message=sip_message)
+    >>> a = parse("<sip:cal3254@192.234.1.12:5060;transport=udp>")
+    >>> a.port == 5060
+    true
+    >>> a.address == "cal3254@192.234.1.12"
+    true
+    >>> a.transport == "udp"
+    true
+    """
+    message = sip_message[5:-1]
+    # cal3254@192.234.1.12:5060;transport=udp
+    if sip_message[:5] != "<sip:":
+        raise Exception("tis not a sip message? :(")
+    loc, tp = message.split(";")
+    transport = tp.split("=")[1]
+    address, port = loc.split(":")
+    return dict(address=address, port=port, transport=transport)
 
 
 
