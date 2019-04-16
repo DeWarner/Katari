b260433a431bd18c52e18b1af83d8a97b1e562fc
dewarner <dec.warner@gmail.com>
2019-04-16 20:03:30 +0100

    fix-doc-tests

--- Katari/sip/parser/__init__.py
+++ Katari/sip/parser/__init__.py
@@ -1,11 +1,19 @@
-
+"""
+>>> a = ParsedMessage.parse("<sip:cal3254@192.234.1.12:5060;transport=udp>")
+>>> a.get("port")
+5060
+>>> a.get("address")
+'cal3254@192.234.1.12'
+>>> a.get("transport")
+'udp'
+"""
 from Katari.sip import SipMessage
 
 
-class SipMessage:
+class ParsedMessage:
     def __init__(self, message):
         self.message = message
-        self.info = SipMessage.parse(message)
+        self.info = self.parse(message)
         self.port = info.get("port")
         self.transport = info.get("transport")
         self.address = info.get("address")
@@ -17,14 +25,6 @@ class SipMessage:
         Takes in a sip message
         :param sip_message:
         :return: dict
-
-        >>> a = parse("<sip:cal3254@192.234.1.12:5060;transport=udp>")
-        >>> a.port == 5060
-        true
-        >>> a.address == "cal3254@192.234.1.12"
-        true
-        >>> a.transport == "udp"
-        true
         """
         message = sip_message[5:-1]
         # cal3254@192.234.1.12:5060;transport=udp
@@ -33,4 +33,4 @@ class SipMessage:
         loc, tp = message.split(";")
         transport = tp.split("=")[1]
         address, port = loc.split(":")
-        return dict(address=address, port=port, transport=transport)
+        return dict(address=address, port=int(port), transport=transport)
