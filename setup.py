4e5e425eedb809acfdb77d287fe60c61de1e846e
dwarner <declan.warner@thevoicefactory.co.uk>
2019-04-18 08:25:19 +0100

    compress doctests into fewer lines

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 import os
 from setuptools import setup,find_packages
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(
     name = "Katari",
