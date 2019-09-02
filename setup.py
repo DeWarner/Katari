fff2ce3a954ed76e03d7798b2882a67336553e13
aparfitt <aaaron.parfitt@thevoicefactory.co.uk>
2019-09-02 10:03:31 +0100

    v0.0.7 - changed python version requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup,find_packages
 
 
 CURRENT_PYTHON = sys.version_info[:2]
-REQUIRED_PYTHON = (3, 6)
+REQUIRED_PYTHON = (3, 4)
 
 # This check and everything above must remain compatible with Python 2.7.
 if CURRENT_PYTHON < REQUIRED_PYTHON:
